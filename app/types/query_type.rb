4409e196cba26c9729dece43f261bbc5e976da2b
João Paulo Lethier <jplethier@gmail.com>
2017-09-10 16:10:17 -0300

    movies list query type added;

--- app/types/query_type.rb
+++ app/types/query_type.rb
@@ -2,6 +2,20 @@ QueryType = GraphQL::ObjectType.define do
   name "Query"
   description "The query root for this schema"
 
+  field :movies do
+    type types[MovieType]
+    argument :year, types.Int
+    argument :partial_title, types.String
+    argument :size, types.Int, default_value: 10
+    resolve -> (obj, args, ctx) {
+      movies = Movie.all
+      movies = movies.where(year: args[:year]) if args[:year].present?
+      movies = movies.where("title like '%#{args[:partial_title].gsub(' ', '%')}%'") if args[:partial_title].present?
+
+      movies.limit(args[:size])
+    }
+  end
+
   field :movie do
     type MovieType
     argument :id, !types.ID
